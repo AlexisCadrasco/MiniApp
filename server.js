f9bdf32eac0078289e9695292e8093c9c659950b
AlexisCadrasco <alexiscadrasco@gmail.com>
2022-11-21 21:07:36 -0500

    New end points

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 let express = require('express'); //Llamar a Express
 let bodyParser = require('body-parser')
+let cors = require('cors')//Control de acceso HTTP
 
 let app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
+app.use(cors())
 
 let port = process.env.PORT || 9890 // Establecemos el puerto de escucha
-
-
 
 app.get('/', (req,res)=>{
   res.json({ mensaje: '¡Hola Mundo!'})
@@ -15,16 +15,26 @@ app.get('/', (req,res)=>{
 
 app.post('/login',(req,res)=>{
     let user = req.body.user;
+    console.log(user);
     let pass = req.body.pass;
+    console.log(pass);
     if(user === 'victor' && pass === '12345'){
-        res.json({mensaje: 'Bienvenido a la aplicación ' + user});
+        res.json({mensaje: 'OK'});
     }else{
-        res.json({mensaje: 'Usuario o password incorrecto.'});
+        res.json({mensaje: 'FAIL'});
     }
     
   });
 
+ app.get('/clients',(req,res)=>{
+  const arr = ['Juan Perez','Ernesto García', 'Pilar Hernandez', 'Pedro Marquez']
+  res.json({clients: arr});
+ });
 
+ app.get('/products',(req,res)=>{
+  const arr = ['Televisor','Nevera', 'Portatil', 'Tablet']
+  res.json({products: arr});
+ })
 
 //Iniciar nuestro server
 app.listen(port);
